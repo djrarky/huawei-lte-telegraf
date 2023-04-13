80536819429d6d8cb26d0f55834644e8b93a9f4d
djrarky <10834935+djrarky@users.noreply.github.com>
2023-04-13 15:05:32 +0100

    fix max_up & max_down variables

--- telegraf-huawei-lte.py
+++ telegraf-huawei-lte.py
@@ -1,62 +1,109 @@
-from argparse import ArgumentParser
-from huawei_lte_api.Client import Client
-from huawei_lte_api.Connection import Connection
+import telnetlib as tn
+import time as t
+import os
+import sys
+import json
 
-p=ArgumentParser()
-p.add_argument('url',type=str)
-p.add_argument('--u',type=str)
-p.add_argument('--p',type=str)
-args=p.parse_args()
+modem_ip = sys.argv[1]
+modem_username = sys.argv[2]
+modem_password = sys.argv[3]
 
-def gather_data(connection):
-    client = Client(connection)
-    deviceSignal = client.device.signal()
-    deviceInformation = client.device.information()
-    smsConfig = client.sms.config()
-    smsCount = client.sms.sms_count()
-    totalTrafficStatistics = client.monitoring.traffic_statistics()
-    totalMonthStatistics = client.monitoring.month_statistics()
-    netCurrentPlan = client.net.current_plmn()
-    monitoringStatus = client.monitoring.status()
-    return deviceSignal, deviceInformation, smsConfig, smsCount, totalTrafficStatistics, totalMonthStatistics, netCurrentPlan, monitoringStatus
+class ParsedStats:
+    def __init__(self, conn_stats_output, system_uptime):
+        conn_stats_output_split = conn_stats_output.decode().split("\r\n")
+        if len(conn_stats_output_split) == 176:
+            self.connection_up = True
+            max_line = conn_stats_output_split[5]
+            match = re.search(r"Upstream rate = (\d+) Kbps, Downstream rate = (\d+) Kbps", max_line)
+            self.max_up = int(match.group(1))
+            self.max_down = int(match.group(2))            
+            current_line = conn_stats_output_split[6].replace("Bearer:\t0, Upstream rate = ", "")
+            current_split = current_line.split(", Downstream rate = ")
+            self.current_up = int(current_split[0].replace(" Kbps", ""))
+            self.current_down = int(current_split[1].replace(" Kbps", ""))
+            snr_line = conn_stats_output_split[16].replace("SNR (dB):\t ", "")
+            snr_split = snr_line.split("\t\t ")
+            self.snr_down = float(snr_split[0])
+            self.snr_up = float(snr_split[1])
+            attn_line = conn_stats_output_split[17].replace("Attn(dB):\t ", "")
+            attn_split = attn_line.split("\t\t ")
+            self.attn_down = float(attn_split[0])
+            self.attn_up = float(attn_split[1])
+            pwr_line = conn_stats_output_split[18].replace("Pwr(dBm):\t ", "")
+            pwr_split = pwr_line.split("\t\t ")
+            self.pwr_down = float(pwr_split[0])
+            self.pwr_up = float(pwr_split[1])
+            interleaving_line = conn_stats_output_split[28].replace("D:\t\t", "")
+            interleaving_split = interleaving_line.split("\t\t")
+            self.int_down = int(interleaving_split[0])
+            self.int_up = int(interleaving_split[1])
+            err_secs_line = conn_stats_output_split[98].replace("ES:\t\t", "")
+            err_secs_split = err_secs_line.split("\t\t")
+            self.err_secs_up = int(err_secs_split[0])
+            self.err_secs_down = int(err_secs_split[1])
+            serious_err_secs_line = conn_stats_output_split[99].replace("SES:\t\t", "")
+            serious_err_secs_split = serious_err_secs_line.split("\t\t")
+            self.serious_err_secs_up = int(serious_err_secs_split[0])
+            self.serious_err_secs_down = int(serious_err_secs_split[1])
+            unavailable_secs_line = conn_stats_output_split[100].replace("UAS:\t\t", "")
+            unavailable_secs_split = unavailable_secs_line.split("\t\t")
+            self.unavailable_secs_up = int(unavailable_secs_split[0])
+            self.unavailable_secs_down = int(unavailable_secs_split[1])
+            self.available_secs = int(conn_stats_output_split[101].replace("AS:\t\t", ""))
+            DSLuptime = conn_stats_output_split[166].replace("Since Link time = ", "")
+            days = hours = minutes = seconds = 0
+            components = DSLuptime.split()
+            for i in range(len(components)):
+                if i > 0 and components[i-1].isdigit():
+                    if "day" in components[i]:
+                        days += int(components[i-1])
+                    elif "hour" in components[i]:
+                        hours += int(components[i-1])
+                    elif "min" in components[i]:
+                        minutes += int(components[i-1])
+                    elif "sec" in components[i]:
+                        seconds += int(components[i-1])
+            days = days
+            hours = hours
+            mins = minutes
+            seconds = seconds
+            self.DSLuptimeSeconds = (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds
+        else:
+            self.connection_up = False
+        system_uptime_split = system_uptime.decode().split("\r\n")
+        self.system_uptime = float(system_uptime_split[1].split(" ")[0]) 
 
-with Connection(args.url, username=args.u, password=args.p) as connection:
-    deviceSignal, deviceInformation, smsConfig, smsCount, totalTrafficStatistics, totalMonthStatistics, netCurrentPlan, monitoringStatus = gather_data(connection)
+def main():
+    parsed_stats = retrieve_stats()
+    influxdb_line = format_influxdb_line(parsed_stats)
+    print(influxdb_line)
 
-# Pull chosen keys
-keys = {
-    "imei": deviceInformation.get('Imei'),
-    "number": smsConfig.get('Sca'),
-    "operator": netCurrentPlan.get('FullName'),
-    "band": deviceSignal.get('band')
-}
-# Pull chosen data
-data = {
-    "band": deviceSignal.get('band'),
-    "uptime": deviceInformation.get('uptime'),
-    "enodeb_id": deviceSignal.get('enodeb_id'),
-    "unreadsms": smsCount.get('LocalInbox'),
-    "currentdownload": totalTrafficStatistics.get('CurrentDownload'),
-    "currentupload": totalTrafficStatistics.get('CurrentUpload'),
-    "totaldownload": totalTrafficStatistics.get('TotalDownload'),
-    "totalupload": totalTrafficStatistics.get('TotalUpload'),
-    "nsa_status": monitoringStatus.get('EndcStatus')
-}
-# Pull chosen signal data
-signal = {
-    "rsrq": deviceSignal.get('rsrq').replace("dB", ""),
-    "rsrp": deviceSignal.get('rsrp').replace("dBm", ""),
-    "rssi": deviceSignal.get('rssi').replace("dBm", ""),
-    "sinr": deviceSignal.get('sinr').replace("dB", ""),
-}
+def retrieve_stats():
+    try:
+        tnconn = tn.Telnet(modem_ip)
+        tnconn.read_until(b"Login:")
+        tnconn.write("{0}\n".format(modem_username).encode())
+        tnconn.read_until(b"Password:")
+        tnconn.write("{0}\n".format(modem_password).encode())
+        tnconn.read_until(b"ATP>")
+        tnconn.write(b"sh\n")
+        tnconn.read_until(b"#")
+        tnconn.write(b"xdslcmd info --stats\n")
+        stats_output = tnconn.read_until(b"#")
+        tnconn.write(b"cat /proc/uptime\n")
+        system_uptime = tnconn.read_until(b"#")
+        parsed_stats = ParsedStats(stats_output, system_uptime)
+        return parsed_stats
+    except Exception:
+        raise
 
-Line1Data = "ltemodem,imei="+keys["imei"]+",number="+keys["number"]+",operator="+keys["operator"]+" "
-Line2Signal = "ltemodem,imei="+keys["imei"]+",number="+keys["number"]+",operator="+keys["operator"]+",band="+keys["band"]+" "
+def format_influxdb_line(parsedStats):
+    try:
+        if parsedStats.connection_up:
+            tags = f"modem_ip={modem_ip}"
+            fields = f"AttDown={parsedStats.attn_down},AttnUp={parsedStats.attn_up},AvailableSecs={parsedStats.available_secs},CurrDown={parsedStats.current_down},CurrUp={parsedStats.current_up},ErrSecsDown={parsedStats.err_secs_down},ErrSecsUp={parsedStats.err_secs_up},InterleavingDown={parsedStats.int_down},InterleavingUp={parsedStats.int_up},MaxDown={parsedStats.max_down},MaxUp={parsedStats.max_up},PwrDown={parsedStats.pwr_down},PwrUp={parsedStats.pwr_up},SeriousErrSecsDown={parsedStats.serious_err_secs_down},SeriousErrSecsUp={parsedStats.serious_err_secs_up},SNRDown={parsedStats.snr_down},SNRUp={parsedStats.snr_up},SystemUptime={parsedStats.system_uptime},UnavailableSecsDown={parsedStats.unavailable_secs_down},UnavailableSecsUp={parsedStats.unavailable_secs_up},DSLuptime={parsedStats.DSLuptimeSeconds}"
+            return f"dslstats,{tags} {fields}"
+    except Exception:
+        raise
 
-for key, value in data.items():
-    Line1Data += key + "=" + value + ","
-for key, value in signal.items():
-    Line2Signal += key + "=" + value + ","
-
-print(Line1Data.strip(","))
-print(Line2Signal.strip(","))
+main()
